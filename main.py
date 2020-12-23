c383b9b400710c912e05046a79828abd0642f516
Rebecca J Proctor <jproctor.m.ed.tn@gmail.com>
2020-12-23 22:02:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+#variables
+user_iq = 190
+user_age = user_iq/4
+a = user_age
+
+# binding the value to the variable
+# aka assigning to a space in memory
+
+# best practices to assigning variables:
+  # snake_case
+  # lowercase w/o sppaces - use under score
+  # letters, numbers underscores
+  # case sensitive
+  # don't overwrite keywords
+
+#constants
+  #pi = 3.14 use all capitals as the convention
+
+PI = 3.14
+
+#dunder - double underscore variables; do not edit
+
+print(user_iq)
+print(a)
+
